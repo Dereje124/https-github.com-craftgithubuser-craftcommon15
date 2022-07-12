94cd49dc683660cc2e2105dab1a7b2b1398bf96d
dagi44 <dagmawisis@gmail.com>
2022-07-12 00:00:39 -0400

    my eclipse change

---
+++ projrct_java_one/src/projrct_java_one_A/class_abc.java
@@ -0,0 +1,12 @@
+package projrct_java_one_A;
+
+public class class_abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("hello abc");
+     System.out.println("hello you");
+     System.out.println("hello every one");
+	}
+
+}
